2c3adc7368b80e2e0b1554dad5265f41efb46c13
Soe Myat Min DESKTOP-5B01SMR <min.s@hundsun.co.jp>
2023-01-25 11:33:39 +0900

    code clean lvl2

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 const hbs = require('hbs');
-var hbsutils = require('hbs-utils')(hbs);
+// var hbsutils = require('hbs-utils')(hbs);
 var cors = require('cors')
 
 var indexRouter = require('./routes/index');
@@ -19,22 +19,22 @@ var app = express();
 app.use(cors({origin: '*'}));
 app.options('*', cors());
 // Define paths for Express config
-// const publicDirectoryPath = path.join(__dirname, '../public')  
+const publicDirectoryPath = path.join(__dirname, '../public')  
 const viewsPath = path.join(__dirname, '/templates/views')
 const partialsPath = path.join(__dirname, '/templates/partials')
 
 // Setup handlebars engine and views location 
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
-// hbs.registerPartials(partialsPath);
-hbsutils.registerPartials(partialsPath);
-hbsutils.registerWatchedPartials(partialsPath);
+hbs.registerPartials(partialsPath);
+// hbsutils.registerPartials(partialsPath);
+// hbsutils.registerWatchedPartials(partialsPath);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/css",express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
 app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
@@ -50,7 +50,7 @@ app.use("/js", express.static(path.join(__dirname, "public/javascripts")));
 
 // app.use("/public/javascripts", express.static(__dirname));
 
-// app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -72,5 +72,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports.handler = serverless(app); 
-// app.listen(3000, () => console.log(`Listening on: 3000`));
 // module.exports = app;
