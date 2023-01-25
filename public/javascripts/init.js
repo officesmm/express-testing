bcd9a17654e2c2792bb27e6f681dabec9272b18b
Soe Myat Min DESKTOP-5B01SMR <min.s@hundsun.co.jp>
2023-01-25 18:58:04 +0900

    WIP: UI table show

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
             para: 1
         }),
         success: function (res) {
-            console.log(JSON.parse(res)[1].name);
+            console.log(JSON.parse(res).cars[1].name);
         }, error: function (jqXHR, textStatus, errorThrown) {
             console.log("jqXHR: " + jqXHR.responseText);
             console.log("textStatus: " + textStatus);
@@ -27,7 +27,7 @@ $(document).ready(function(){
             para: 1
         },
         success: function (res) {
-            console.log(JSON.parse(res)[0].name);
+            console.log(JSON.parse(res).cars[0].name);
         }, error: function (jqXHR, textStatus, errorThrown) {
             console.log("jqXHR: " + jqXHR.responseText);
             console.log("textStatus: " + textStatus);
@@ -35,5 +35,9 @@ $(document).ready(function(){
         },
     });
     console.log("if the car is shown ajax is working");
+
+    console.log($("#tab").val());
+    $("#sidemenu_" + $("#tab").val()).css("display", "block");
+    $("#sidemenu_" + $("#tab").val() + "_" + $("#selection").val()).addClass("active");
 });
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+// function taber(){
+//   $("#sidemenu_" + table).css("display", "block");
+//   $("#sidemenu_" + table + "_" + table).addClass("active");
+// }
+
+taber = () => {
+  $("#sidemenu_" + table).css("display", "block");
+  $("#sidemenu_" + table + "_" + table).addClass("active");
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('admin/admin', { 
-    title: 'Login' });
+    title: 'Login',
+    tab: 'table',
+    selection: 'table',
+    // proPlayer: taber()
+  });
 });
 
 module.exports = router;
